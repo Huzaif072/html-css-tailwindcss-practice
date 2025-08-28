630797b23c47ba8ccb68b16373625433e3f90656
Muhammad Huzaif Amir <alih95745@gmail.com>
2025-08-28 08:24:24 +0500

    commit

--- Local Library (Express.js)/routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-// Example user API endpoint
-router.get('/', (req, res) => {
-  res.json({ message: 'User API endpoint' });
-});
-
-export default router;
